8e8c5c6015e097d956da85660168d00513f10d4b
gem4ik <mgemskij@gmail.com>
2025-03-10 23:26:51 +0300

    deploy testing

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import { getSidebarLayout } from "@/app/layouts/sidebarLayout/sidebarLayout"
 
 export default function Home() {
-  return <div> home </div>
+  return <div>home </div>
 }
 
 Home.getLayout = getSidebarLayout
