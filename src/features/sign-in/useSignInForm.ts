f8cc45680cdaa393379d75dd32874ba2ddd46982
Alexandr1991Sh <an1991sh@yandex.ru>
2025-03-12 21:34:15 +0300

    .gitignore -(/pnpm-lock.yaml) SignInForm

---
+++ src/features/sign-in/useSignInForm.ts
@@ -0,0 +1,37 @@
+import { useEffect, useRef } from 'react'
+import { useForm } from 'react-hook-form'
+import { useTranslation } from '@/shared/hooks/useTranslation'
+
+export interface SignInFormType {
+    email: string
+    password: string
+}
+
+
+export const useSignInForm = () => {
+    const { t } = useTranslation()
+    const {
+        clearErrors,
+        control,
+        formState: { errors, isValid },
+        handleSubmit,
+        reset,
+        trigger,
+    } = useForm<SignInFormType>({
+        defaultValues: { email: '' },
+        mode: 'onSubmit',
+    })
+    const hasInteracted = useRef(false)
+
+    useEffect(() => {
+        reset({ email: '', password: '' })
+        clearErrors()
+    }, [t])
+
+    const onFieldChange = async (fieldName: keyof SignInFormType) => {
+        hasInteracted.current = true
+        await trigger(fieldName)
+    }
+
+    return { control, errors, handleSubmit, isValid, onFieldChange, trigger }
+}
---
+++ src/shared/ui/Card/Card.tsx
@@ -0,0 +1,21 @@
+import { ComponentPropsWithoutRef, ElementRef, ElementType, forwardRef } from 'react'
+
+import { cn } from '@/shared/utils/cn'
+
+type Props<T extends ElementType = 'div'> = {
+    className?: string
+} & ComponentPropsWithoutRef<T>
+
+export const Card = forwardRef<ElementRef<'div'>, Props>(
+    ({ children, className, ...props }, ref) => {
+        return (
+            <div
+                className={cn('bg-dark-500 min-w-[100px] min-h-[100px] rounded border-dark-300', className)}
+                ref={ref}
+                {...props}
+            >
+                {children}
+            </div>
+        )
+    }
+)
