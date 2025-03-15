931c70d206e6f463d9c74effec550c09b4d905ab
gem4ik <mgemskij@gmail.com>
2025-03-15 22:11:55 +0300

    refactor pagination params hook

--- src/features/users-list/UsersList.tsx
+++ src/features/users-list/UsersList.tsx
@@ -5,28 +5,53 @@ import { useGetUsersQuery } from "@/queries/allUsers/usersgenerated"
 import { useTranslation } from "@/shared/hooks/useTranslation"
 import Pagination from "@/shared/ui/pagination/Pagination"
 import Select from "@/shared/ui/Select/Select"
-import {useUsersPaginationCopy} from "@/features/users-list/hooks/useUsersPaginationCopy";
-import SearchWithQueries from "@/shared/ui/SearchWithQueries/SearchWithQueries";
+import { usePaginationParams } from "@/shared/hooks/usePaginationParams"
+import SearchWithQueries from "@/shared/ui/SearchWithQueries/SearchWithQueries"
+import { Option } from "@/shared/ui/Select/types"
+import { UserBlockStatus } from "@/types"
+import { useRouter } from "next/router"
 
 export const UsersList = () => {
+  const router = useRouter()
   const { t } = useTranslation()
-  const {
-    sortHandler,
-    sortDirection,
-    onByBanFilterChange,
-    paginationParams,
-    byBanSelectValue,
-    filterByBanOptions,
-  } = useUsersPaginationCopy()
+  const { sortHandler, sortDirection, paginationParams } = usePaginationParams()
+
+  const onByBanFilterChange = (e: Option) => {
+    const oldQueries = { ...router.query }
+    if (e.value === UserBlockStatus.All) {
+      delete oldQueries.statusFilter
+    }
+    const queries =
+      e.value === UserBlockStatus.All
+        ? { ...oldQueries, pageNumber: 1 }
+        : {
+            ...oldQueries,
+            statusFilter: e.value as UserBlockStatus,
+            pageNumber: 1,
+          }
+    void router.push({
+      pathname: router.pathname,
+      query: queries,
+    })
+  }
+
+  const filterByBanOptions = [
+    { label: t.admin.userList.banValue.notSelected, value: "ALL" },
+    { label: t.admin.userList.banValue.blocked, value: "BLOCKED" },
+    { label: t.admin.userList.banValue.notBlocked, value: "UNBLOCKED" },
+  ]
+
+  const byBanSelectValue =
+    filterByBanOptions.find(
+      option => option.value === paginationParams.statusFilter
+    ) || filterByBanOptions[0]
 
   const { data: users } = useGetUsersQuery({ variables: paginationParams })
 
   return (
     <div className="flex flex-col gap-8 pb-[40px]">
       <div className="px-4 pt-4 flex gap-40">
-        <SearchWithQueries
-          placeholder={t.sidebar.search}
-        />
+        <SearchWithQueries placeholder={t.sidebar.search} />
         <Select
           options={filterByBanOptions}
           onChange={e => onByBanFilterChange(e)}
---
+++ src/shared/hooks/usePaginationParams.ts
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react"
+
+import { defaultPaginationParams } from "@/features/users-list/CONSTs"
+import { SortDirection, UserBlockStatus } from "@/types"
+import { useRouter } from "next/router"
+
+export const usePaginationParams = () => {
+  const router = useRouter()
+
+  const [paginationParams, setPaginationParams] = useState(
+    defaultPaginationParams
+  )
+
+  useEffect(() => {
+    setPaginationParams({
+      ...paginationParams,
+      sortBy: router.query.sortBy ? (router.query.sortBy as string) : "",
+      sortDirection: router.query.sortDirection
+        ? (router.query.sortDirection as SortDirection)
+        : SortDirection.Asc,
+      searchTerm: router.query.searchTerm
+        ? (router.query.searchTerm as string)
+        : "",
+      statusFilter: router.query.statusFilter
+        ? (router.query.statusFilter as UserBlockStatus)
+        : UserBlockStatus.All,
+      pageSize: router.query.pageSize ? Number(router.query.pageSize) : 10,
+      pageNumber: router.query.pageNumber ? Number(router.query.pageNumber) : 1,
+    })
+  }, [router])
+
+  const sortHandler = (e: { direction: "asc" | "desc"; key: string }) => {
+    void router.push({
+      pathname: router.pathname,
+      query: {
+        ...router.query,
+        sortBy: e.key,
+        sortDirection: e.direction,
+      },
+    })
+  }
+
+  const sortDirection = {
+    direction: paginationParams.sortDirection,
+    key: paginationParams.sortBy,
+  }
+
+  return {
+    sortHandler,
+    sortDirection,
+    setPaginationParams,
+    paginationParams,
+  }
+}
