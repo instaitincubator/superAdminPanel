b3487a64ddef10c29d2bd1b6d9dc66cf59c033e4
gem4ik <mgemskij@gmail.com>
2025-03-27 18:52:56 +0300

    deploy commit

--- src/pages/payments/index.tsx
+++ src/pages/payments/index.tsx
@@ -1,10 +1,10 @@
 import React from "react"
 
 import { getSidebarLayout } from "@/app/layouts/sidebarLayout/sidebarLayout"
-import {PaymentsList} from "@/features/payments-list/PaymentsList";
+import { PaymentsList } from "@/features/payments-list/PaymentsList"
 
 export default function Payments() {
-  return <div><PaymentsList/> </div>
+  return <PaymentsList />
 }
 
 Payments.getLayout = getSidebarLayout
